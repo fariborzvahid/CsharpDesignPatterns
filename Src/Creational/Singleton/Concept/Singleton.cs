2471fa3aaaf907ce94376b770b16edab4c208c76
Fariborz Vahid <fariborzvahid@gmail.com>
2023-04-30 14:01:24 +0330

    (feat) [Singleton] : Add double check singleton class
    Add singleton class with double check strategy for thread safe

---
+++ Src/Creational/Singleton/Concept/Singleton.cs
@@ -0,0 +1,30 @@
+﻿namespace CsharpDesignPatterns.Concept;
+
+public class  Singleton
+{
+    private Singleton() {
+        SingletonTestValue = "Text for testing singleton pattern";
+    }
+
+    private static Singleton? _instance;
+    public string SingletonTestValue { get; set; }
+
+    private static readonly object _lock = new object();
+    public static Singleton GetInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }                
+            }
+            return _instance;
+        }
+    }
+}
---
+++ Test/DesignPatternsTest/SingletonTest.cs
@@ -0,0 +1,15 @@
+namespace DesignPatternsTest
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void Test_value()
+        {
+
+        }
+        public void Test_Thread()
+        {
+
+        }
+    }
+}
